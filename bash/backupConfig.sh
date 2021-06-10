d0eeb7429900a6f9df5b3996635b5e2ad9e95096
shile <13051960278@163.com>
2021-06-10 17:34:52 +0800

    update `date +%FT%T`

--- bash/backupConfig.sh
+++ bash/backupConfig.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# 1. 统计要备份的数据和文件，形成表格，便于记录
+# 2. 在服务端创建好需要的备份目录
+# 3. 在需要备份的服务器上添加定时任务，用于数据的归档备份和汇总
+# 4. 在需要备份的服务器上添加定时任务，用于同步到服务端
+# 5. 在服务端查看对应目录下步骤4同步的文件
+# 6. 使用checkBackupStatus.sh脚本，查看每个同步周期变化的文件，用于检查备份任务是否正常执行
+
 ## 使用docker方式运行nextcloudcmd, 需要提前在服务端创建对应目录 https://github.com/le-shi/nextcloud-client-docker
 # docker pull juanitomint/nextcloud-client:alpine_3.6
-# docker tag juanitomint/nextcloud-client:alpine_3.6 cr.example.com/3rd/nextcloud-client:alpine_3.6
-# docker run --rm -v /tmp/ooo:/media/nextcloud -e NC_EXIT=true -e NC_USER=username -e NC_PASS=password -e NC_URL=http://backup.example.com/remote.php/webdav/test cr.example.com/3rd/nextcloud-client:alpine_3.6
-## nextcloudcmd
+# docker tag juanitomint/nextcloud-client:alpine_3.6 cr.github.com/3rd/nextcloud-client:alpine_3.6
+# nextcloudcmd for docker
+# 05 * * * * docker run --rm -v /home/centos/nextcloud_back:/media/nextcloud -e NC_HIDDEN=true -e NC_EXIT=true -e NC_USER=用户名 -e NC_PASS=密码 -e NC_URL=http://backup.github.com/remote.php/webdav/9111_lab/nextcloud_back cr.github.com/3rd/nextcloud-client:alpine_3.6
 # 附件的备份
 # 05 * * * * volume/nginx/data
 # 数据库备份文件的备份
@@ -13,9 +20,9 @@
 # 05 * * * * volume/svn-data
 # 环境配置文件的备份
 # 05 * * * * nextcloud_back
-# 
-# do_check 方法是聚合之前的条件检查
-# 下面的所有 do_sync 方法是把一台机器上需要备份的文件(小文件，不算数据库备份文件，svn仓库，附件)聚合到一个目录，通过 nextcloudcmd 同步到备份服务器
+
+# do_check 方法是聚合(汇总)之前的条件检查
+# do_sync 方法是把一台机器上需要备份的文件(小文件，不算数据库备份文件，svn仓库，附件)聚合到一个目录，再通过 nextcloudcmd 同步到备份服务器
 # back_jobs_xml: 
 back_dir=/home/centos/nextcloud_back
 config_dir=/home/centos
@@ -88,6 +95,7 @@ do_sync_autostart_script(){
     job_back_dir=${back_dir}/autostart_script
     mkdir -pv ${job_back_dir}
     rsync -zva /etc/init.d/*.sh ${job_back_dir}
+    rsync -zva /etc/rc.d/rc.local ${job_back_dir}
     let count+=1
 }
 
@@ -138,6 +146,15 @@ do_sync_redis_conf(){
     let count+=1
 }
 
+do_sync_custome_file(){
+    # 第1个参数备份出来的目录名 第2个参数是目标目录或文件,设计通配符的需要用双引号引起来
+    job_back_dir=${back_dir}/${1}
+    mkdir -pv ${job_back_dir}
+    src_path=$(eval echo ${2})
+    rsync -zva ${src_path} ${job_back_dir}
+    let count+=1
+}
+
 main(){
     do_check_command_exist rsync
     do_check_command_result
@@ -153,6 +170,24 @@ main(){
     # do_sync_nginx_conf
     # do_sync_mysql_conf
     # do_sync_redis_conf
+
+    # do_sync_custome_file harbor_config "${config_dir}/harbor/{*.sh,*.yml,prepare}"
+    # do_sync_custome_file harbor_ssl /etc/harbor/ssl
+    # do_sync_custome_file jenkins-scripts ${config_dir}/jenkins/volume/jenkins-scripts
+    # do_sync_custome_file prometheus_config "${config_dir}/prometheus/{*.yml,*.yaml,*.tmpl,readme,reload}"
+    # do_sync_custome_file ingress_config ${config_dir}/ingress
+    # do_sync_custome_file sonarqube_config "${config_dir}/sonarqube/*.yaml"
+    # do_sync_custome_file gitlab_config /root/gitlab/gitlabhq-11.10.1/config
+    # do_sync_custome_file autostart_script "/root/{README,start}"
+    # do_sync_custome_file named_config "/etc/named.*"
+    # do_sync_custome_file named_master_zone_config "/var/named/{dynamic,*.zone}"
+
+    # do_sync_custome_file nginx_config /root/nginx
+    # do_sync_custome_file prom_node_config /root/prom
+    # do_sync_custome_file minio_config /root/minio
+    # do_sync_custome_file git_config /root/git
+    # do_sync_custome_file svn_config /root/svn
+    # do_sync_custome_file mysql_config /root/mysql
 
     if [[ ${count} -eq 0 ]]
     then
--- docker/prometheus/readme.md
+++ docker/prometheus/readme.md
@@ -1,12 +1,13 @@
 # 声明
 
-> 启动前先把prometheus挂在的TSDB目录进行授权: `chown -R 65534.65534 ./volume/prometheus/data`
+> 启动前先把prometheus挂在的TSDB目录进行授权: `chown -R 65534.65534 ./volume/prometheus`
 
 ---
 
 1. 告警规则来自: <https://awesome-prometheus-alerts.grep.to> 里面部分规则没有的是通过官方插件或文档获取的
    1. 规则获取日期: 2020-12-03
    2. 日期之后的规则可能会有更新,自己可进行对比或Fork此项目进行修改
+   3. 官方收集的exporter: <https://prometheus.io/docs/instrumenting/exporters/>
 2. 部分规则进行了简单的修改,可根据自己实际的情况进行调试
 3. 告警规则文件: alertmanager-rules.yml
 4. 当前规则包含:
@@ -16,10 +17,10 @@
    4. blackbox([prom/blackbox-exporter](https://hub.docker.com/r/prom/blackbox-exporter))-[9965]
    5. mysql([prom/mysqld-exporter](https://hub.docker.com/r/prom/mysqld-exporter))-[7362]
    6. redis([oliver006/redis_exporter](https://hub.docker.com/r/oliver006/redis_exporter))-[763]
-   7. rabbitmq([rabbitmq的prometheus插件,3.8.9之后自动开启(15692)](https://www.rabbitmq.com/prometheus.html))-[10991]
-   8. ceph([ceph的prometheus模块,需手动开启(9283)](https://docs.ceph.com/en/latest/mgr/prometheus/))-[2842]
-   9. jenkins([jenkins的prometheus插件,需手动安装(8080/prometheus)](https://plugins.jenkins.io/prometheus/))-[9524]
-   10. jvm/eureka([MavenPlugin](./docs/jvm.md))-[4701]
+   7. rabbitmq([rabbitmq的prometheus插件,3.8.9之后自动开启(port: 15692)](https://www.rabbitmq.com/prometheus.html))-[10991]
+   8. ceph([ceph的prometheus模块,需手动开启(port: 9283)](https://docs.ceph.com/en/latest/mgr/prometheus/))-[2842]
+   9. jenkins([jenkins的prometheus插件,需手动安装(port: 8080/prometheus)](https://plugins.jenkins.io/prometheus/))-[9524]
+   10. jvm([MavenPlugin](./docs/jvm.md))-[4701]
 5. 关于报警时间的问题
     1. Prometheus 告警自定义模板的默认使用的是`UTC`时间
     2. 改成北京时间,其中 `Add 28800e9`  就是表示加8个小时。[28800e9为什么表示8小时?](https://www.google.com/search?q=28800e9%E4%B8%BA%E4%BB%80%E4%B9%88%E8%A1%A8%E7%A4%BA8%E5%B0%8F%E6%97%B6%3F&oq=28800e9%E4%B8%BA%E4%BB%80%E4%B9%88%E8%A1%A8%E7%A4%BA8%E5%B0%8F%E6%97%B6%3F&aqs=chrome..69i64j69i57.2040j0j1&sourceid=chrome&ie=UTF-8)
@@ -37,7 +38,7 @@
    1. openstack([niedbalski/openstack-exporter-linux-amd64](https://quay.io/niedbalski/openstack-exporter-linux-amd64))-[9701]
    2. zipkin([zipkin内置的prometheus接口](https://github.com/le-shi/docker-zipkin))-[1598]
    3. harbor([zhangguanzhang/harbor_exporter](https://hub.docker.com/r/zhangguanzhang/harbor_exporter))
-   4. docker([google/cadvisor](https://hub.docker.com/r/google/cadvisor))
+   4. docker-container([google/cadvisor](https://hub.docker.com/r/google/cadvisor))
    5. kubernetes-[13105]
    6. ingress-nginx([参考](https://github.com/kubernetes/ingress-nginx/tree/master/deploy/prometheus))-[9614]
    7. nginx-log-[13844]
